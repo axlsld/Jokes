293fd8e7c2732b122fcf14326f6e3fa828676225
alexis <johnalexissaldua06@gmail.com>
2026-03-27 14:53:46 +0800

    added json file feature and separated each functions

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Joke.ts is where you can input a number to read a joke from the csv file.
 writeJoke.ts is where you can write a joke to insert in the csv file.
+
+.env is ignored so replace the placeholders on .env.example file with their real value.
--- writeJoke.js
+++ writeJoke.js
@@ -1,41 +1,97 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
-var filePath = './csv/joke.csv';
-fs.readFile(filePath, 'utf8', function (err, data) {
-    if (err) {
-        console.error('Error reading the file: ' + err.message);
-        return;
+var promptSync = require("prompt-sync");
+var dotenv = require("dotenv");
+dotenv.config();
+var prompt = promptSync();
+function fileSelection() {
+    console.log("---------------------------------------");
+    console.log("This is a program that makes you write");
+    console.log("a joke.");
+    console.log("\nChoose the type of file you want:");
+    console.log("- Type 1 for csv\n- Type 2 for json\n- Type anything else to exit.");
+    console.log("---------------------------------------");
+    var choice = prompt('Enter a number: ');
+    var path;
+    if (choice == '1') {
+        if (!process.env.CSV_PATH) {
+            throw new Error("No CSV_PATH in .env");
+        }
+        path = process.env.CSV_PATH;
     }
-    var promptSync = require('prompt-sync')();
+    else if (choice == '2') {
+        if (!process.env.JSON_PATH) {
+            throw new Error("No JSON_PATH in .env");
+        }
+        path = process.env.JSON_PATH;
+    }
+    else {
+        console.log('Exiting the program...');
+        path = 'end';
+    }
+    return path;
+}
+function addJokeCSV(filePath, input) {
+    var _a;
+    var data = fs.readFileSync(filePath, 'utf8');
     var rows = data.split('\n');
-    var csvArray = rows.map(function (row) { return row.split(','); });
-    while (true) {
-        var lastRow = csvArray[csvArray.length - 1];
-        var lastId = parseInt(lastRow[0]);
-        var newId = lastId + 1;
-        console.log("---------------------------------------");
-        console.log("            Add A NEW JOKE");
-        console.log("         or type \"0\" to exit");
-        console.log("---------------------------------------");
-        var input = promptSync('Enter your joke: ');
-        var safeInput = input.replace(/"/g, '""');
-        var newJoke = "\n".concat(newId, ",\"").concat(safeInput, "\"");
-        if (input != '0') {
-            try {
-                fs.appendFileSync(filePath, newJoke);
-                console.log("\n***************************************");
-                console.log("Joke no.".concat(newId, " added successfully!"));
-                console.log("***************************************");
-            }
-            catch (err) {
-                console.error("Error writing file: " + err.message);
-            }
-            csvArray.push([newId.toString(), input]);
-        }
-        else {
-            console.log('Exiting the program...');
-            break;
+    var lastRow = (_a = rows[rows.length - 1]) === null || _a === void 0 ? void 0 : _a.split(',');
+    var lastId = parseInt(lastRow[0]);
+    var newId = lastId + 1;
+    var safeInput = input.replace(/"/g, '""');
+    var newJoke = "\n".concat(newId, ",\"").concat(safeInput, "\"");
+    fs.appendFileSync(filePath, newJoke);
+    console.log("\n***************************************");
+    console.log("Joke no.".concat(newId, " added successfully!"));
+    console.log("***************************************");
+}
+function addJokeJSON(filePath, input) {
+    var data = fs.readFileSync(filePath, 'utf8');
+    var json = JSON.parse(data);
+    var newId = 1;
+    if (json.length > 0) {
+        var lastItem = json[json.length - 1];
+        var lastId = Number(lastItem.id);
+        if (!isNaN(lastId)) {
+            newId = lastId + 1;
         }
     }
-});
+    json.push({ id: newId, joke: input });
+    fs.writeFileSync(filePath, JSON.stringify(json, null, 2));
+    console.log("\n***************************************");
+    console.log("Joke no.".concat(newId, " added successfully!"));
+    console.log("***************************************");
+}
+while (true) {
+    var pathChosen = fileSelection();
+    if (pathChosen === 'end') {
+        break;
+    }
+    if (!fs.existsSync(pathChosen)) {
+        console.log("File not found.");
+        continue;
+    }
+    console.log("---------------------------------------");
+    console.log("            Add A NEW JOKE");
+    console.log("         or type \"0\" to exit");
+    console.log("---------------------------------------");
+    var input = prompt('Enter your joke: ');
+    if (input != '0') {
+        try {
+            if (pathChosen === process.env.CSV_PATH) {
+                addJokeCSV(pathChosen, input);
+            }
+            else {
+                addJokeJSON(pathChosen, input);
+            }
+        }
+        catch (err) {
+            console.error("Error writing file: " + err.message);
+        }
+    }
+    else {
+        console.log('Exiting the program...');
+        break;
+    }
+}
